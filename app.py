0db22b1e2df80fbf555569eeff426c8ef9787e9c
perarasuempire-netizen <perarasuempire@gmail.com>
2026-08-08 20:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -527,7 +527,7 @@ def create_payment(order_id):
     return jsonify({
         "order_id": razorpay_order['id'],
         "amount": amount,
-        "key": RAZORPAY_KEY_ID
+        "key": rzp_test_TLf997naPo8AsG
     })
 
 @app.route('/verify_payment', methods=['POST'])
